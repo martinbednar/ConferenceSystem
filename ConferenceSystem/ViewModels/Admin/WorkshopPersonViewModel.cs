d4d8267a19a19add4773b8cec2780c4b7febc81e
martinbednar <martin.bedn20@gmail.com>
2018-04-28 23:42:50 +0200

    Admin edit Workshops created

--- ConferenceSystem/ViewModels/Admin/WorkshopPersonViewModel.cs
+++ ConferenceSystem/ViewModels/Admin/WorkshopPersonViewModel.cs
@@ -1,7 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ConferencySystem.BL.DTO;
+using ConferencySystem.BL.Services;
+using DotVVM.Framework.Runtime.Filters;
+
 namespace ConferencySystem.ViewModels.Admin
 {
+    [Authorize(Roles = new[] { "admin", "super" })]
     public class WorkshopPersonViewModel : MainMasterPageViewModel
     {
+        public int CurrentUserId
+        {
+            get
+            {
+                return Convert.ToInt32(Context.Parameters["PersonId"]);
+            }
+        }
+
+        public List<WorkshopsBlockDTO> WorkshopsBlocks { get; set; }
+
+        public bool Alert { get; set; } = false;
+
+        public override Task PreRender()
+        {
+            var workshopService = new WorkshopService();
+            WorkshopsBlocks = workshopService.GetWorkshopsBlocks(CurrentUserId);
+
+            bool anyWorkshopRegistered = false;
+
+            AppUserDTO actualUser = workshopService.GetUser(CurrentUserId);
+
+            if (actualUser != null)
+            {
+                foreach (WorkshopsBlockDTO workshopBlock in WorkshopsBlocks)
+                {
+                    foreach (WorkshopDTO workshop in workshopBlock.Workshops)
+                    {
+                        foreach (AppUserDTO person in workshop.People)
+                        {
+                            if (person.Id == CurrentUserId)
+                            {
+                                workshop.Registered = true;
+                                anyWorkshopRegistered = true;
+                            }
+                        }
+                    }
+                    if (anyWorkshopRegistered)
+                    {
+                        workshopBlock.AnyWorkshopRegistered = true;
+                    }
+                    anyWorkshopRegistered = false;
+                }
+            }
+            //}
+            return base.PreRender();
+        }
+
+        public void Register(int workshopId)
+        {
+            var workshopService = new WorkshopService();
+            if (!workshopService.RegisterWorkshop(CurrentUserId, workshopId))
+            {
+                Alert = true;
+            }
+        }
+
+        public void Unregister(int workshopId)
+        {
+            var workshopService = new WorkshopService();
+            workshopService.UnregisterWorkshop(CurrentUserId, workshopId);
+        }
     }
 }
 
