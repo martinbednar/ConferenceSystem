50dbaeeeb4cc94ff7ca6e65643b4c0dbdd26ade7
martinbednar <martin.bedn20@gmail.com>
2018-04-30 16:45:56 +0200

    EmailService Added

---
+++ ConferenceSystem.BL/Services/EmailService.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace ConferencySystem.BL.Services
+{
+    public class EmailService
+    {
+        public void SendEmail(string receiverEmail, string subject, string body)
+        {
+            var msg = new MailMessage("nakopnetesvojiskolu@gmail.com", receiverEmail, subject, body);
+
+            msg.IsBodyHtml = true;
+            var smtpClient = new SmtpClient("smtp.gmail.com", 587)
+            {
+                UseDefaultCredentials = true,
+                Credentials = new NetworkCredential("nakopnetesvojiskolu@gmail.com", "KonfSysEmail123"),
+                EnableSsl = true
+            };
+            smtpClient.Send(msg);
+        }
+    }
+}
