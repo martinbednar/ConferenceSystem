f1c63cb3d62c687c18183fc73e1a9b2583a9f7c1
martinbednar <martin.bedn20@gmail.com>
2018-08-23 16:58:23 +0200

    Repair Password reset

--- ConferenceSystem.BL/Services/ResetPasswordService.cs
+++ ConferenceSystem.BL/Services/ResetPasswordService.cs
@@ -16,20 +16,22 @@ namespace ConferencySystem.BL.Services
         {
             AppUserManager userManager = new AppUserManager(new DbContext());
             var provider = new DpapiDataProtectionProvider("KonferencniSystem");
-            userManager.UserTokenProvider = new DataProtectorTokenProvider<AppUser, int>(provider.Create("EmailReset"));
+            //userManager.UserTokenProvider = new DataProtectorTokenProvider<AppUser, int>(provider.Create("EmailReset"));
+            userManager.UserTokenProvider = new EmailTokenProvider<AppUser, int>();
             userManager.ResetPassword(userId, token, newPassword);
         }
 
         public void SendResetPasswordEmail(AppUserDTO dataUser)
         {
+            EmailService emailService = new EmailService();
+
             var provider = new DpapiDataProtectionProvider("KonferencniSystem");
             var appUserManager = new AppUserManager(new DbContext());
-            appUserManager.UserTokenProvider = new DataProtectorTokenProvider<AppUser, int>(provider.Create("EmailReset"));
-            
+            //appUserManager.UserTokenProvider = new DataProtectorTokenProvider<AppUser, int>(provider.Create("EmailReset"));
+            appUserManager.UserTokenProvider = new EmailTokenProvider<AppUser, int>();
+
             string token = appUserManager.GeneratePasswordResetToken(dataUser.Id);
             var callbackUrl = new Uri(string.Format(HttpContext.Current.Request.Url + "/reset?userid={0}&token={1}", dataUser.Id, HttpUtility.UrlEncode(token)));
-            
-            EmailService emailService = new EmailService();
 
             string mailBodyhtml =
                 "<p>Zaznamenali jsme požadavek na obnovení hesla ke Konferenčnímu systému festivalu vzdělání Nakopněte svoji školu (<a href=\"http://nakopnetesvojiskolu.azurewebsites.net/\">http://nakopnetesvojiskolu.azurewebsites.net/</a>).</p><br><p>Pro obnovení hesla klikněte na odkaz:</p><br><p><a href=\"" + callbackUrl + "\">OBNOVIT HESLO</a></p><br><p>Pokud se Vám nedaří na odkaz kliknout, překopírujte do adresního řádku prohlížeče následující adresu: <a href=\"" + callbackUrl + "\">" + callbackUrl + "</a></p><br>Pro získání aktuálních informací můžete sledovat náš <a href=\"http://www.nakopnetesvojiskolu.cz/\" target=\"_blank\">web</a> nebo <a href=\"https://www.facebook.com/nakopnete.svoji.skolu/\" target=\"_blank\">Facebook</a>.";
