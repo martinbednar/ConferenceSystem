78233b8a40752e1ba50682342ec6c99968f81698
martinbednar <xbedna60@fit.vutbr.cz>
2019-10-31 16:53:53 +0100

    Remove old no-longer used method for sending emails.

--- ConferenceSystem/ViewModels/Admin/UsersViewModel.cs
+++ ConferenceSystem/ViewModels/Admin/UsersViewModel.cs
@@ -84,36 +84,6 @@ namespace ConferencySystem.ViewModels.Admin
             adminService.DeleteUser(id);
         }
 
-        public void SendEmails()
-        {
-            var loginService = new LoginService();
-
-            List<AppUserDTO> allUsers = loginService.GetAllUsers();
-
-            string MailBodyhtml;
-
-            var smtpClient = new SmtpClient("smtp.gmail.com", 587)
-            {
-                UseDefaultCredentials = true,
-                Credentials = new NetworkCredential(Secrets.GmailZN, Secrets.GmailZNPwd),
-                EnableSsl = true
-            }; //if your from email address is "from@hotmail.com" then host should be "smtp.hotmail.com"**
-
-            foreach (AppUserDTO user in allUsers)
-            {
-                MailBodyhtml =
-                "<p>Vážený úèastníku/ Vážená úèastnice festivalu vzdìlání Nakopnìte svoji školu,</p><p>dnes <b>v 16:00</b> se spustí registrace wokshopù a pøednášek. Zatím si mùžete prohlédnout témata, která jsou na výbìr. Tato registrace bude uzavøena <b>9.2. ve 12:00.</b></p><p>Registraci urèitì neodkládejte, protože workshopy mají omezenou kapacitu a po naplnìní kapacity již nebude možné se na workshop pøihlásit.</p><p>V každém èasovém bloku si vyberte <b>jednu</b> pøednášku èi workshop.</p><p>Výbìr mùžete uskuteènit v Konferenèním systému, pøes který probíhala registrace (<a href=\"http://nakopnetesvojiskolu.azurewebsites.net/workshop\" target=\"_blank\">http://nakopnetesvojiskolu.azurewebsites.net/workshop</a>).</p><p>Pøipomínáme ještì výbìr stravování, které je otevøeno <b>do 8.2. do 12:00</b>.<p><p>Tìšíme se na Vás.</p><br><p>Mùžete sledovat náš <a href=\"http://www.nakopnetesvojiskolu.cz/\" target=\"_blank\">web</a> nebo <a href=\"https://www.facebook.com/nakopnete.svoji.skolu/\" target=\"_blank\">Facebook</a>.</p>";
-
-                var msg = new MailMessage(Secrets.GmailZN, user.Email, "Nakopnìte svoji školu - registrace workshopù", MailBodyhtml);
-                msg.Bcc.Add(new MailAddress("martinbedn20@seznam.cz"));
-                msg.ReplyToList.Add(new MailAddress(Secrets.GmailZN));
-                msg.IsBodyHtml = true;
-                smtpClient.Send(msg);
-
-                loginService.EmailSent(user.Id);
-            }
-        }
-
         public void ExportToExcel()
         {
             var exporter = new GridViewExportExcel<AppUserDTO>(new GridViewExportExcelSettings<AppUserDTO>());
